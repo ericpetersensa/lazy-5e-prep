2830396e52a4f323a35c2a3df43694a30b6e7a8c
Eric Petersen <ericpetersensa@gmail.com>
2025-08-20 09:07:38 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
+import { registerSettings, openSettingsMenu } from "./settings.js";
+import { createLazy5eJournal } from "./journal/generator.js";
 
+Hooks.once("init", () => {
+  console.debug("lazy-5e-prep | init");
+  registerSettings();
+});
+
+Hooks.once("ready", async () => {
+  console.debug("lazy-5e-prep | ready");
+});
+
+globalThis.Lazy5ePrep = {
+  createJournal: createLazy5eJournal,
+  openSettings: openSettingsMenu
+};
